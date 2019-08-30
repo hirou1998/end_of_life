336c9531e90fefcd118773be366d83a01b9922fe
hirou1998 <tanahi1025@gmail.com>
2019-08-30 13:14:54 +0900

    my ending note list and menu modified

---
+++ app/Controller/my_ending_note_list/my_ending_note.php
@@ -0,0 +1,25 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Model\Dao\User;
+
+// TOPページのコントローラ
+$app->get('/my_ending_note_list/', function (Request $request, Response $response) {
+
+    $data = $request->getQueryParams();
+
+    // $this->db->select("");
+    $sql = "select * from user inner join my_ending_note on user.id = my_ending_note.user_id;";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+
+    $list = $stmt->fetchAll();
+
+    $data =[
+        "list" => $list,
+    ];
+
+    // Render index view
+    return $this->view->render($response, 'my_ending_note_list/my_ending_note_list.twig', $data);
+});
